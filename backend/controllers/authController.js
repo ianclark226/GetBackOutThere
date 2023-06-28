cc3e79e554fd684ce027e1009501fc8468923210
Ian Clark <ianclark226@gmail.com>
2023-06-28 13:44:53 -0400

    install vercel

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,7 +14,7 @@ authController.post('/register', async(req, res) => {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         const newUser = await User.create({ ...req.body, password: hashedPassword})
         const { password, ...others} = newUser._doc
-        const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {expiresIn: '4h'})
+        const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {expiresIn: '8d'})
 
         return res.status(201).json({ others, token})
     } catch (err) {
