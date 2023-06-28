ca2722fd12b4066d87b6371a99a1acfdbe07919c
Ian Clark <ianclark226@gmail.com>
2023-06-28 10:57:25 -0400

    took out send file path

--- backend/index.js
+++ backend/index.js
@@ -34,9 +34,9 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
   
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+//   app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+//   });
 
 
 
